6349c20681aa3d86fa0939b2e84190cbfe15862b
Wcy-wyy <2324418649@qq.com>
2021-12-17 16:04:49 +0800

    A:执行shell命令工具类

---
+++ src/main/java/com/mohan/utils/ExCallShellUtils.java
@@ -0,0 +1,53 @@
+package com.mohan.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class ExCallShellUtils {
+    /**
+     * 执行shell命令
+     *
+     * @param shell 命令
+     * @param workspace 路径
+     */
+    public static void callShell(String shell, String... workspace){
+        try {
+            File dir = null;
+            if(workspace[0] != null){
+                dir = new File(workspace[0]);
+                System.out.println(workspace[0]);
+            }
+            Process process = Runtime.getRuntime().exec(shell, null, dir);
+            int status = process.waitFor();
+            if(status != 0){
+                System.err.println("Failed to call shell's command and the return status's is: " + status);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void callScript(String script, String... workspace){
+        try {
+            String cmd = "sh " + script;
+            File dir = null;
+            if(workspace[0] != null){
+                dir = new File(workspace[0]);
+                System.out.println(workspace[0]);
+            }
+            String[] evnp = {"val=2", "call=Bash Shell"};
+            Process process = Runtime.getRuntime().exec(cmd, evnp, dir);
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            input.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
