bd9a50ff949e58bccd090ce3b9a8e85c9601c25b
Wcy-wyy <2324418649@qq.com>
2021-12-20 13:44:51 +0800

    A:代码生成工具

---
+++ src/main/java/com/mohan/utils/Generator.java
@@ -0,0 +1,131 @@
+package com.mohan.utils;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.converts.OracleTypeConvert;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * zhjav
+ */
+@Slf4j
+public class Generator {
+
+
+    public static void main(String[] args) {
+
+        String db_className = "com.mysql.cj.jdbc.Driver";
+        String db_url = "jdbc:mysql://121.199.35.237:3306/smart_countryside?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
+        String db_username = "root";
+        String db_password = "dRx9QZfRHC4R";
+
+
+        String dirModule = "";
+
+        String dirParent = "com.mohan.module";
+        String dirEntity = "domain";
+        String dirMapper = "mapper";
+        String dirService = "service";
+        String dirServiceImpl = "service.impl";
+        String dirMapperXml = "resources/mapper";
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir") ;
+        gc.setOutputDir(projectPath + "/src/main/code");
+
+        gc.setAuthor("wyy");
+        gc.setOpen(false);
+
+        gc.setBaseColumnList(true);
+        gc.setBaseResultMap(true);
+        gc.setFileOverride(true);
+        mpg.setGlobalConfig(gc);
+
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setTypeConvert(new OracleTypeConvert());
+        dsc.setUrl(db_url);
+        dsc.setDriverName(db_className);
+        dsc.setUsername(db_username);
+        dsc.setPassword(db_password);
+        mpg.setDataSource(dsc);
+
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(dirModule);
+        pc.setParent(dirParent);
+        pc.setEntity(dirEntity);
+        pc.setMapper(dirMapper);
+        pc.setService(dirService);
+        pc.setServiceImpl(dirServiceImpl);
+//        pc.setXml(dirMapperXml);
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/"+dirModule+File.separator+ tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        // 配置自定义输出模板
+
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+
+        strategy.setInclude(
+                "user_role"
+        );
+
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
